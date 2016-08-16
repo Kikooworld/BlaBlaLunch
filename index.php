69c904365d1ffe91be0ced7c59f5ebc24ce28307
Dorine Mazeyrat <dorine.mazeyrat@persistent.com>
2016-08-16 17:42:56 +0200

    Header and Footer as PHP includes

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- En-tête de la page -->
+		<meta charset="utf-8" />
+		<link rel="icon" href="img/favicon.ico" />
+		<link rel="stylesheet" href="style.css" />
+		<title>Blabla Lunch test</title>
+	</head>
+
+	<body>
+		<header>
+			<!-- Placez ici le contenu de l'en-tête de votre page -->
+			<?php include('header.html'); ?>
+		</header>
+
+		<section>
+			<div id="header_hr"/>
+		</section>
+
+		<section>
+			<!-- Corps de la page -->		
+			<p>This is a section</p>
+		</section>
+
+		<footer>
+			<?php include('footer.html'); ?>
+		</footer>
+	</body>
+</html>
