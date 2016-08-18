84db240600f29cd4a2590cdf4c80070c8531adbf
daradinh <dara-tiffanie.dinh@persistentsas.com>
2016-08-18 13:35:13 +0200

    function get cars available

--- command/get_cars.php
+++ command/get_cars.php
@@ -8,44 +8,48 @@
 		
 		//preparer la requete
 		$req_pre = mysqli_prepare($db,
-		
-		"SELECT cars.id, cars.owner, cars.time, cars.seats, restaurants.id, restaurants.name
-		FROM cars, restaurants
-		WHERE cars.restaurant_id = restaurants.id;") or die(mysqli_error($db));
+		   "SELECT cars.id, cars.owner, cars.time, cars.seats, restaurants.id, restaurants.name
+			FROM cars, restaurants
+			WHERE cars.restaurant_id = restaurants.id;") or die(mysqli_error($db));
 		
 		mysqli_set_charset( $db, 'utf8' );
 		
 		//executer la requete
 		mysqli_stmt_execute($req_pre);
 		
-		//stocker le résultat
-		mysqli_stmt_store_result($req_pre);
-		
-		/* Libère le résultat */
-		mysqli_stmt_free_result($stmt);
-		
 		// Association des variables de résultat
 		mysqli_stmt_bind_result($req_pre, $carId, $carOwner, $carTime, $carSeats, $restaurantId, $restaurantName);
 
+		//stocker le résultat
+		mysqli_stmt_store_result($req_pre);
+		
 		//lire la requete
 		while (mysqli_stmt_fetch($req_pre)) 
 		{
-					$req_pre2 = mysqli_prepare($db, "SELECT COUNT(*) as participationsNb FROM participations WHERE participations.car_id = '$carId;'") or die(mysqli_error($db));
+			$req_pre2 = mysqli_prepare($db, "SELECT COUNT(*) FROM participations WHERE participations.car_id = '$carId;'") or die(mysqli_error($db));
 					
-					//executer la requete
-					mysqli_stmt_execute($req_pre2);
-					
-					// Association des variables de résultat
-					mysqli_stmt_bind_result($req_pre2, $participationsNb);
-		
-					echo "<tr>";
-					echo "<td>".$restaurantName."</td>";
-					echo "<td>".$carTime."</td>";
-					echo "<td>".$carOwner."</td>";
-					echo "<td>".$participationsNb."//".$carSeats."</td>";
-					echo "<td><input type=\"submit\" value=\"Réserver une place\" /></td>";
+			//executer la requete
+			mysqli_stmt_execute($req_pre2);
+			
+			// Association des variables de résultat
+			mysqli_stmt_bind_result($req_pre2, $participationsNb);
+			
+			//lecture de la requete
+			mysqli_stmt_fetch($req_pre2);
+			
+			$availableSeats = $carSeats - $participationsNb;
+
+			echo "<tr>";
+			echo "<td>".$restaurantName."</td>";
+			echo "<td>".$carTime."</td>";
+			echo "<td>".$carOwner."</td>";
+			echo "<td>".$availableSeats."/".$carSeats."</td>";
+			echo "<td><input type=\"submit\" value=\"Réserver une place\" /></td>";
 			echo "</tr>";
 		}
+		
+		/* Libère le résultat */
+		mysqli_stmt_free_result($req_pre);
 		
 		// Fermeture de la commande
 		mysqli_stmt_close($req_pre);
