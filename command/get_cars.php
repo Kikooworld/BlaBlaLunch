f76461a07ff333a363c46f416fb5a99fd153e228
daradinh <dara-tiffanie.dinh@persistentsas.com>
2016-08-22 11:36:07 +0200

    use php array for sql request

--- command/get_cars.php
+++ command/get_cars.php
@@ -1,39 +1,47 @@
 <?php
 	function BuildHtml($db, $request, $useReservedSeats)
 	{
-		$req_pre = mysqli_prepare($db, $request) or die(mysqli_error($db));
-		
 		mysqli_set_charset( $db, 'utf8' );
-		mysqli_stmt_execute($req_pre);
-		mysqli_stmt_bind_result($req_pre, $carId, $carOwner, $carTime, $carSeats, $carTakeaway, $restaurantId, $restaurantName, $reservedSeats);
-
-		while (mysqli_stmt_fetch($req_pre)) 
-		{						
-			$availableSeats = $carSeats;
+		
+		$result = mysqli_query($db, $request);
+		if (!$result)
+		{
+			die(mysqli_error($db));
+		}
+		
+		$rows = [];
+		while($row = mysqli_fetch_array($result))
+		{
+			$rows[] = $row;
+		}
+		
+		foreach($rows as &$row)
+		{			
+			$availableSeats = $row['carSeats'];
 			if ($useReservedSeats)
 			{
-				$availableSeats = $carSeats - $reservedSeats;
+				$availableSeats = $row['carSeats'] - $row['participationsCount'];
 			}
 			$checked = "";
-			if ($carTakeaway == 1)
+			if ($row['carTakeaway'] == 1)
 			{
 				$checked =  "checked";
 			}
 
 			echo "<form action=\"book_a_seat.php\" method=\"post\">";
 			echo "<tr>";
-			echo "<td>".$restaurantName."</td>";
+			echo "<td>".$row['restaurantName']."</td>";
 			echo "<td><input type=\"checkbox\"  disabled ".$checked."></td>";
-			echo "<td>".$carTime."</td>";
-			echo "<td>".$carOwner."</td>";
-			echo "<td>".$availableSeats."/".$carSeats."</td>";
-			echo "<td><input type=\"hidden\" name=\"carId\" value=\"".$carId."\"></td>";
+			echo "<td>".$row['carTime']."</td>";
+			echo "<td>".$row['carOwner']."</td>";
+			echo "<td>".$availableSeats."/".$row['carSeats']."</td>";
+			echo "<td><input type=\"hidden\" name=\"carId\" value=\"".$row['carId']."\"></td>";
 			echo "<td><input type=\"submit\" value=\"Réserver une place\"/></td>";
 			echo "</tr>";
 			echo "</form>";
 		}
 		
-		mysqli_stmt_close($req_pre);
+		// mysqli_stmt_close($req_pre);
 	}
 
 	function GetCars()
@@ -44,7 +52,7 @@
 		$db = ConnectToDataBase();
 		
 		//Requete pour récupérer les voitures sans passagers
-		BuildHtml($db, "SELECT cars.id, cars.owner, cars.time, cars.seats, cars.take_away, cars.restaurant_id, restaurants.name, COUNT(*)
+		BuildHtml($db, "SELECT cars.id AS carId, cars.owner AS carOwner, cars.time AS carTime, cars.seats AS carSeats, cars.take_away AS carTakeaway, cars.restaurant_id AS restaurantId, restaurants.name AS restaurantName, COUNT(*) AS participationsCount
 			FROM cars, restaurants
 			WHERE cars.restaurant_id = restaurants.id AND cars.id NOT IN
 			  ( SELECT cars.id
@@ -53,7 +61,7 @@
 				GROUP BY cars.id)
 			GROUP BY cars.id;", false);
 		
-		BuildHtml($db, "SELECT cars.id, cars.owner, cars.time, cars.seats, cars.take_away, cars.restaurant_id, restaurants.name, COUNT(*)
+		BuildHtml($db, "SELECT cars.id AS carId, cars.owner AS carOwner, cars.time AS carTime, cars.seats AS carSeats, cars.take_away AS carTakeaway, cars.restaurant_id AS restaurantId, restaurants.name AS restaurantName, COUNT(*) AS participationsCount
 			FROM cars, restaurants, participations
 			WHERE cars.restaurant_id = restaurants.id AND cars.id = participations.car_id
 			GROUP BY cars.id;", true);
