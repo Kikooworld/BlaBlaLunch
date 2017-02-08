2b13371b05e82333f3ba12eee76fef1c79a8fe4e
daradinh <dara-tiffanie.dinh@persistentsas.com>
2017-02-08 16:59:17 +0100

    [Story] Check Security of MySQL requests when inserting a new car

--- command/check_existing_car.php
+++ command/check_existing_car.php
@@ -18,36 +18,5 @@
 	setcookie("restaurant", $restaurant_id, time() + (86400 * 30), '/');
 	setcookie("takeaway", $take_away, time() + (86400 * 30), '/');
 	
-	//Connect to database
-	$db = ConnectToDataBase();
-	
-	//Check if car already exists
-	$req_pre = mysqli_prepare($db, "SELECT cars.id FROM cars WHERE cars.owner = ?;") or die(mysqli_error($db));
-    mysqli_stmt_bind_param($req_pre, "s", $name);
-	mysqli_stmt_execute($req_pre);
-	mysqli_stmt_bind_result($req_pre, $existingId);
-	mysqli_stmt_fetch($req_pre);
-	
-	//Disconnect from database
-	DisconnectFromDatabase($db);
-	
-	if ($existingId == "")
-	{
-		InsertACar($name, $restaurant_id, $seats, $time, $take_away);
-	}
-	else
-	{
-		$message = "Un covoitureur avec le même conducteur existe déjà dans la base de données. Voulez-vous mettre à jour les données ?";
-		echo "<script>
-		var update = confirm(\"$message\");
-		if (update)
-		{
-			location.href = \"update_car.php?id=$existingId&restaurant=$restaurant_id&seats=$seats&time=$time&away=$take_away\";
-		}
-		else
-		{
-			window.location=\"../suggest_a_blablalunch.php\";
-		}
-		</script>";
-	}
+	InsertACar($name, $restaurant_id, $seats, $time, $take_away);
 ?>
--- command/insert_a_car.php
+++ command/insert_a_car.php
@@ -7,16 +7,51 @@
 		//Connect to database
 		$db = ConnectToDataBase();
 		
-		//preparer la requete
-		$req_pre = mysqli_prepare($db, "INSERT INTO `cars` (`owner`, `time`, `seats`, `id`, `restaurant_id`, `take_away`) VALUES (?, \"" . $time . "\", \"" . $seats . "\", NULL, \"" . $restaurant_id . "\", \"" . $take_away . "\");") or die(mysqli_error($db));
+        
+        //Check if car already exists
+        $req_pre = mysqli_prepare($db, "SELECT cars.id FROM cars WHERE cars.owner = ?;") or die(mysqli_error($db));
         mysqli_stmt_bind_param($req_pre, "s", $name);
-			
-		//executer la requete
-		mysqli_stmt_execute($req_pre);
-		
-		//Disconnect from database
-		DisconnectFromDatabase($db);
-		
-		header('Location: ../index.php');
+        mysqli_stmt_execute($req_pre);
+        mysqli_stmt_bind_result($req_pre, $existingId);
+        mysqli_stmt_fetch($req_pre);
+
+        if ($existingId == "")
+        {
+            //preparer la requete
+            $req_pre2 = mysqli_prepare($db, "INSERT INTO `cars` (`owner`, `time`, `seats`, `id`, `restaurant_id`, `take_away`) VALUES (?, \"" . $time . "\", \"" . $seats . "\", NULL, \"" . $restaurant_id . "\", \"" . $take_away . "\");") or die(mysqli_error($db));
+            mysqli_stmt_bind_param($req_pre2, "s", $name);
+                
+            //executer la requete
+            mysqli_stmt_execute($req_pre2);
+            
+            //Unlock table
+            mysqli_query($db, "UNLOCK TABLES;");
+            
+            //Disconnect from database
+            DisconnectFromDatabase($db);
+            
+            header('Location: ../index.php');
+        }
+        else
+        {
+            //Unlock table
+            mysqli_query($db, "UNLOCK TABLES;");
+            
+            //Disconnect from database
+            DisconnectFromDatabase($db);
+            
+            $message = "Un covoitureur avec le même conducteur existe déjà dans la base de données. Voulez-vous mettre à jour les données ?";
+            echo "<script>
+            var update = confirm(\"$message\");
+            if (update)
+            {
+                location.href = \"update_car.php?id=$existingId&restaurant=$restaurant_id&seats=$seats&time=$time&away=$take_away\";
+            }
+            else
+            {
+                window.location=\"../suggest_a_blablalunch.php\";
+            }
+            </script>";
+        }
 	}
 ?>
